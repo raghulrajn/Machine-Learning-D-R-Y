0b2677fa56e662dfafd495924ed140b2e962a2ee
Raghul Raj <76444923+raghulrajn@users.noreply.github.com>
2022-04-26 23:55:21 +0530

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -1,12 +1,20 @@
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
+features = "[<INDEPENDENT FEATURES>]"
 
-def createPCA(variance = 0.95,df):
+X = df.loc[:,features].values
+y = df.loc[:,[TARGET]].values
+
+_X = StandardScaler().fit_transform(X)
+
+components = 2
+def createPCA(components,df=_X):
   
-    pca = PCA(variance) # selecting PCA components with retaining 95% variance in dara
+    pca = PCA(n_components = components) # selecting PCA components with retaining 95% variance in dara
     principalComponents = pca.fit_transform(df) 
     cols = []
-    for i in range(len(principalComponents)):
+    for i in range(len(components)):
         cols.append("pc"+str(i+1))
     principalDf = pd.DataFrame(data = principalComponents
                  , columns = cols)
